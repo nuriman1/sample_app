4bee61d27d209c924ba031d5a9e9a1e88fa7ee95
nuriman1 <imanrasid98@gmail.com>
2020-08-18 16:30:20 +0800

    Init Project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   validates :name, presence: true, 
     length: { maximum: Settings.validations.name.max_length }
 
@@ -51,6 +51,19 @@ end
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.password_expired_time.hours.ago
   end
 
   private
